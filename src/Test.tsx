28a85df98e250bec6853b89f5648d3a1ee69d876
Patrick Kuntz <Patrick.Kuntz@gmx.net>
2022-10-13 07:20:40 +0000

    Added Buttons to Test

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import { Button } from "@mui/material";
+import React, { useState } from "react";
 
 const Test = () => {
+  const [count, setCount] = useState<number>(0);
   return (
     <>
       <h1>Im the test headline!</h1>
+      <div>
+        <Button variant="contained">Increase Count</Button>
+      </div>
+      <div>
+        <Button variant="contained">Decrease Count</Button>
+      </div>
     </>
   );
 };
