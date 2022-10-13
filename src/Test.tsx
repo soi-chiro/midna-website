fd07f96a35e3ff85fb9a68b5d467e326ada9d295
Patrick Kuntz <Patrick.Kuntz@gmx.net>
2022-10-13 07:26:19 +0000

    Update Test.tsx

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,15 +1,22 @@
-import { Button } from "@mui/material";
+import { Button, makeStyles } from "@mui/material";
 import React, { useState } from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    buttonContainer: {},
+  })
+);
 
 const Test = () => {
+  const classes = useStyles();
+
   const [count, setCount] = useState<number>(0);
   return (
     <>
-      <h1>Im the test headline!</h1>
+      <h1>Im the test headline and i contain the number: {count}</h1>
       <div>
         <Button variant="contained">Increase Count</Button>
-      </div>
-      <div>
         <Button variant="contained">Decrease Count</Button>
       </div>
     </>
